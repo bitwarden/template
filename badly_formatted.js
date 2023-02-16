6e910d3e8562ba314e035444d4d6900d4de28db7
Matt Gibson <mgibson@bitwarden.com>
2023-02-16 16:53:31 -0500

    Test if prettier config overrides

--- badly_formatted.js
+++ badly_formatted.js
@@ -1,1 +1,19 @@
-export function fromB64ToArray(str){if(str==null){return null}if(Utils.isNode){return new Uint8Array(Buffer.from(str,"base64"))}else{const binaryString=Utils.global.atob(str);const bytes=new Uint8Array(binaryString.length);for(let i=0;i<binaryString.length;i+=1){bytes[i]=binaryString.charCodeAt(i)}return bytes}}
+class Utils {
+   static isNode = false;
+}
+
+export function fromB64ToArray(str) {
+   if (str == null) {
+      return null;
+   }
+   if (Utils.isNode) {
+      return new Uint8Array(Buffer.from(str, "base64"));
+   } else {
+      const binaryString = Utils.global.atob(str);
+      const bytes = new Uint8Array(binaryString.length);
+      for (let i = 0; i < binaryString.length; i += 1) {
+         bytes[i] = binaryString.charCodeAt(i);
+      }
+      return bytes;
+   }
+}
