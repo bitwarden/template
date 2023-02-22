1730cc371b12e3771602a0ffa25adde127574a56
MGibson1 <MGibson1@users.noreply.github.com>
2023-02-22 14:09:21 +0000

    Apply lint fixes

--- badly_formatted.js
+++ badly_formatted.js
@@ -1,23 +1,23 @@
 class Utils {
-   static isNode = false;
+  static isNode = false
 }
 
-export function test() {
-   return Utils.isNode;
+export function test () {
+  return Utils.isNode
 }
 
-export function fromB64ToArray(str) {
-   if (str == null) {
-      return null;
-   }
-   if (Utils.isNode) {
-      return new Uint8Array(Buffer.from(str, "base64"));
-   } else {
-      const binaryString = Utils.global.atob(str);
-      const bytes = new Uint8Array(binaryString.length);
-      for (let i = 0; i < binaryString.length; i += 1) {
-         bytes[i] = binaryString.charCodeAt(i);
-      }
-      return bytes;
-   }
+export function fromB64ToArray (str) {
+  if (str == null) {
+    return null
+  }
+  if (Utils.isNode) {
+    return new Uint8Array(Buffer.from(str, 'base64'))
+  } else {
+    const binaryString = Utils.global.atob(str)
+    const bytes = new Uint8Array(binaryString.length)
+    for (let i = 0; i < binaryString.length; i += 1) {
+      bytes[i] = binaryString.charCodeAt(i)
+    }
+    return bytes
+  }
 }
